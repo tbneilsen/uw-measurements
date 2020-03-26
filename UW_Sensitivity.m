65d81c4545c3c0746272d7a75194070875c99bb1
herrick9 <herrick9@byu.edu>
2020-03-26 20:41:27 +0000

    Replace UW_Sensitivity.m, added ability to calculate transfer function of environment

--- UW_Sensitivity.m
+++ UW_Sensitivity.m
@@ -1,4 +1,4 @@
-function new_spec = UW_Sensitivity(f, Xss, sens, source, mic) 
+function [new_spec,H] = UW_Sensitivity(f, Xin, Xout, sens, ambient, source, mic) 
 
 % Requires inputs of frequency array and single-sided spectrum from
 % my_autospec function. Also requires the sensitivity value put in
@@ -22,7 +22,7 @@ if mic == 4034
 end
 
 if mic == 4038
-    f_mic = 1000.*hydrophone_4038(:,1);
+    f_mic = 10^6.*hydrophone_4038(:,1);
     dB_mic = hydrophone_4038(:,2);
 end
 
@@ -31,8 +31,21 @@ if source == 4038
     dB_source = projector_4038(:,2);
 end 
 
+if source == 4034
+    f_source = 1000.*projector_4034(:,1);
+    dB_source = projector_4034(:,2);
+end
+
 
 % now interpolate sensitivity data to fit data size of measurement'f1'
-    dB_interp = interp1(f_mic,dB_mic,f, 'spline','extrap');
+mic_interp = interp1(f_mic,dB_mic,f, 'spline','extrap');
+source_interp = interp1(f_source,dB_source,f, 'spline','extrap');
 
-    new_spec = Xss*sens./(1e9.*10.^(dB_interp./20));
+new_spec = Xout*sens./(1e9.*10.^(mic_interp./20));
+H = (Xout./(10.^(mic_interp./20).*10.^(source_interp./20).*(Xin+ambient))).^-1;
+    
+return
+
+    
+    
+    
