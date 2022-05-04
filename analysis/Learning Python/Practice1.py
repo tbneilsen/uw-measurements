b6fb8ff05a9d1ebf638ce1646a106d4d7e26c5a5
Alex Hopps <alexhoppsalot@gmail.com>
2022-05-04 16:51:22 -0600

    more plotting practice

--- analysis/Learning Python/Practice1.py
+++ analysis/Learning Python/Practice1.py
@@ -4,11 +4,17 @@ Author: Alex Hopps
 '''
 #%%
 from matplotlib import pyplot as plt
+import numpy as np
 
 x_vals = [1, 2, 3, 4]
 y_vals = [5, 4, 6, 2]
-
+x = np.linspace(-2*np.pi,2*np.pi,600)
+plt.figure()
 plt.plot(x_vals, y_vals)
+plt.plot(x,np.sin(x))
+plt.title("label")
 
-plt.show()
+plt.savefig("C:/Users/khopp/OneDrive/Documents/plot2.jpg")
+# %%
+
 # %%
---
+++ analysis/Learning Python/practice2.py
@@ -0,0 +1,6 @@
+print("Hello World")
+age = 10
+print(age)
+
+import numpy as np 
+print(np.sin(2))
