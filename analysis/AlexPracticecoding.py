6ff8043cffee6cfbb3f4ea5f7322b76215efcd25
Alex Hopps <alexhoppsalot@gmail.com>
2022-05-04 17:27:06 -0600

    More practice of coding

---
+++ analysis/AlexPracticecoding.py
@@ -0,0 +1,8 @@
+#%%
+import ESAUdata as ed
+import numpy as np
+import sys
+sys.path.append("C:/Users/khopp/UW-Research-Files/byuarglib")
+import byuarglib as byu
+byu.crest(np.linspace(0,100,100))
+# %%
--- analysis/Learning Python/practice2.py
+++
@@ -1,6 +0,0 @@
-print("Hello World")
-age = 10
-print(age)
-
-import numpy as np 
-print(np.sin(2))
