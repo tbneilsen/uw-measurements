63cb8998ddb29c7f28470f1ea1e58b8cc98bbce7
Alex Hopps <alexhoppsalot@gmail.com>
2022-05-04 18:09:12 -0600

    numpy matrix math tutorial

--- analysis/Learning Python/Practice1.py
+++ analysis/Learning Python/Practice1.py
@@ -16,5 +16,49 @@ plt.title("label")
 
 plt.savefig("C:/Users/khopp/OneDrive/Documents/plot2.jpg")
 # %%
+import numpy as np
+a= np.array([1, 2, 3, 4, 5])
+print(a)
+print(a.shape)
+print(a.dtype)
+print(a.ndim)
+print(a.size)
+print(a.itemsize)
+
+# %%
+a = np.array([1,2,3])
+print(a[0])
+a[0]=5
+print(a[0])
+b = a * np.array([2,0,2])
+print(b)
+print(b.sum())
+
+# %%
+#functions applied to a numpy array are like matrix math, whereas normal lists just append numbers
+l=[1,2,3]
+a=np.array([1,2,3])
+
+l2=2*l
+a2=2*a
+print(l2) #apends an aaray to the end
+print(a2) #multiplies each element
+# %%
+dot=np.dot(a,b)
+print(dot)
+cross=np.cross(a,b)
+print(cross)
+
+# %%
+a = np.array([[1,2],[3,4]])
+print(a.shape)
+print(a)
+print(a[0,:]) #the : means all column in the 0 row; you can also put a number there (0) to get the first column
+# %%
+bool = a > 2
+print(a[bool]) #prints the elements of the array that meet the condition 
+b = np.where(a>2, a, -1) #replace all elements that don't meet the condition with -1
+print(b)
+# %%
 
 # %%
